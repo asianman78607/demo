7286df54611aa5c923dc5ed0083ff4747ca149a8
asianman78607 <112815565+asianman78607@users.noreply.github.com>
2022-09-12 14:14:43 +0530

    host & debug

--- flipkart/settings.py
+++ flipkart/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&uw*ef095937y3&4dd5nt0@h#^8$s($m8(ut@z4l+-!4+^8@px'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['asianman78607.herokuapp.com']
 
 
 # Application definition
